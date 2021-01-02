43ab730eafa10c0d4fabd7b9890383c300341ce2
jakusoba <jane.akusoba@gmail.com>
2021-01-02 16:49:57 -0500

    Edited main.cpp to remove personal information

--- main.cpp
+++ main.cpp
@@ -21,7 +21,8 @@ int main()
 {
     const string studentData[] =
 
-    {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Obianuju,Akusoba,oakusob@wgu.edu,21,10,20,30,SOFTWARE"
+    {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        "A5,Obianuju,Akusoba,heythere@example.com,21,10,20,30,SOFTWARE"
         
     };
     
@@ -30,7 +31,7 @@ int main()
     
 
     cout << "C867 Scripting and Programming Applications." << endl;
-    cout << "Written in C++ by Obianuju Akusoba, Student ID: 001348128" << endl;
+    cout << "Written in C++ by Obianuju Akusoba." << endl;
  
     
     cout << endl;
